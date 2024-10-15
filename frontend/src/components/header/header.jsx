1b36b01f36eac23361179e9518c185df18a7ca2f
junaid <junaidsheikh45@gmail.com>
2024-10-15 12:48:55 +0530

    logout functionality added

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,32 +1,48 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useState } from 'react'
+import { authAction } from '../../store/auth'
 import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 function Header() {
-
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+        const [ userData, setUserData] = useState();
     useEffect(() => {
         const fetchUserDetails = async () => {
             const res = await axios.get("http://localhost:5000/api/v1/user-details", {
                 withCredentials: true
             })
-            console.log(res);
+            setUserData(res.data.user);
         }
         fetchUserDetails();
     }, [])
 
+    const logoutHandler = async () => {
+        const res = await axios.post("http://localhost:5000/api/v1/logout", {
+            withCredentials : true
+        })
+        console.log(res);
+        dispatch(authAction.logout());
+        navigate("/")
+    }
+
     return (
-        <div className='bg-green-900 rounded py-8 flex flex-col md:flex-row items-center justify-center gap-4 md:justify-between px-4 lg:px-12'>
+        <>
+        {userData && <div className='bg-green-900 rounded py-8 flex flex-col md:flex-row items-center justify-center gap-4 md:justify-between px-4 lg:px-12'>
             <div className="flex flex-col items-center md:items-start">
                 <p className='text-zinc-300'>Profile</p>
-                <h1 className='text-3xl md:text-4xl lg:text-5xl text-zinc-100 font-bold text-center'>the code master</h1>
-                <p className=''>junaid@gmail.com</p>
+                <h1 className='my-4 text-3xl md:text-4xl lg:text-5xl text-zinc-100 font-bold text-center'>{userData.username}</h1>
+                <p className='text-zinc-300 mt-1'>{userData.email}</p>
             </div>
             <div className="text-zinc-300 mt-1">
-                <button className='bg-white px-4 py-2 rounded text-zinc-800 font-semibold hover:shadow-xl transition-all duration-300'>
+                <button onClick={logoutHandler} className='bg-white px-4 py-2 rounded text-zinc-800 font-semibold hover:shadow-xl transition-all duration-300'>
                     Logout
                 </button>
             </div>
-        </div>
+        </div>}
+        </>
     )
 }
 
